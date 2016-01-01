64c54ed04755effbbddb14eafd2a123b9659016c
jphuangjr <jphuangjr@gmail.com>
2015-12-31 17:05:28 -0800

    working on Links and http requests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,13 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.addLink = function(){
+    Links.addLink();
+    $scope.getLinks();
+  };
+  $scope.getLinks = function(){
+    $scope.data = Links.getLinks();
+  };
+  $scope.data = {};
+
 });
