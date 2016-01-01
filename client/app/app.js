9fb72426de82363a7f3b16cdb2b058a14f2926b1
jphuangjr <jphuangjr@gmail.com>
2015-12-31 20:13:01 -0800

    Finished WOOOOOOOO

--- client/app/app.js
+++ client/app/app.js
@@ -18,6 +18,11 @@ angular.module('shortly', [
     .when('/links', {
       templateUrl: 'app/links/links.html',
       controller: 'LinksController'
+    })
+
+    .when('/shorten', {
+      templateUrl: 'app/shorten/shorten.html',
+      controller: 'ShortenController'
     });
 
     // Your code here
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,15 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.data = {};
+  $scope.link = {};
+
+  $scope.addLink = function(){
+    Links.addOne($scope.linkInput);
+  };
+  $scope.getAll = function(){
+    Links.getAll().then(function(data){
+      $scope.data.links = data;
+    });
+  };
 });
